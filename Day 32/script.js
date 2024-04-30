0312f9f6787962af861cace0c7613140272c3a44
Sujal <sujal.kumar311@gmail.com>
2024-04-30 22:07:42 +0530

    Made a filter based small project using javascript

---
+++ Day 32/script.js
@@ -0,0 +1,21 @@
+const tabs = document.querySelector(".tabs")
+const gallery = document.querySelectorAll(".images")
+
+tabs.addEventListener("click", (event) => {
+    console.log(event.target.dataset.category);
+    if (event.target.dataset.category !== undefined) {
+        filterSearch(event.target.dataset.category);
+    }
+
+})
+
+const filterSearch = (value) => {
+    gallery.forEach((currElem) => {
+        // console.log(currElem.dataset.category);
+        if (currElem.dataset.category === value || value === "all") {
+            currElem.style.display = "block"
+        } else {
+            currElem.style.display = "none"
+        }
+    })
+}
